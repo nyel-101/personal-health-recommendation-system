487231df59433f3956863891e511a280cd4f9b76
nyel-101 <dandevjr@gmail.com>
2025-02-28 00:34:25 +0000

    refactor(config): sanitize auth tokens

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ef8f50c6a4404206a01a1f1e1ce211b9 = () => {
-  return '2026-04-08T07:09:21Z';
+const node_f39d4ea1697149a4bfb733b4de174af2 = () => {
+  return '2025-02-28T00:34:25Z';
 };
