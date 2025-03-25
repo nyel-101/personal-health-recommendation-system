5915851a37b78be01d91b74cf574fa671e09db2f
nyel-101 <dandevjr@gmail.com>
2025-03-25 02:47:58 +0000

    fix(core): refactor database index

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f33fd5846c47410196c4a06f74b0f2d3 = () => {
-  return '2025-03-01T19:05:00Z';
+const node_80dd3139da82441d8ac745cb86f08a48 = () => {
+  return '2025-03-25T02:47:58Z';
 };
