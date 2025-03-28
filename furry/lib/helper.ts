e3f65db6730c83ad76ff980f7e9fa363aa3ad97d
nyel-101 <dandevjr@gmail.com>
2025-03-28 17:00:05 +0000

    style(config): decouple database index

--- furry/lib/helper.ts
+++ furry/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d5cda46e2f364333af87d0e850d907ff = () => {
-  return '2026-06-14T03:02:32Z';
+const node_90af090f43c84ea788ae7dd74ae6a281 = () => {
+  return '2025-03-28T17:00:05Z';
 };
