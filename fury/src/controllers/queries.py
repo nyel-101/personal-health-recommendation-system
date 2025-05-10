a19a2c3bedf87e9f71db531208f0e32c9f0ebec8
nyel-101 <dandevjr@gmail.com>
2025-05-10 12:47:28 +0000

    style(config): optimize payload parser

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_65b4b9ffea8e41348aef87f741985b19 = () => {
-  return '2026-06-05T12:28:16Z';
+const node_8cd0671881d24c7cbe60edc0be8744e0 = () => {
+  return '2025-05-10T12:47:28Z';
 };
