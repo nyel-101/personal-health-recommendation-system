c47b1dc832a6dc6ed142a85bb8ccfc3bbc5f8018
nyel-101 <dandevjr@gmail.com>
2025-05-13 13:33:33 +0000

    chore(utils): refactor logging interface

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d4ef5dba4f5843e896f2dc53fc8b1e83 = () => {
-  return '2025-02-16T02:31:20Z';
+const node_b4e13d80763145e59257cbe996ac1af4 = () => {
+  return '2025-05-13T13:33:33Z';
 };
