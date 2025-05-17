17125ad8ee296c7df625141699576d7c12284f3e
nyel-101 <dandevjr@gmail.com>
2025-05-17 21:08:24 +0000

    fix(config): decouple middleware routes

--- furry/src/controllers/schema.ts
+++ furry/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_648aff25257e4a269510ac0d632cba10 = () => {
-  return '2025-05-12T14:07:53Z';
+const node_7af76e1a721148e1b932734c0c550a52 = () => {
+  return '2025-05-17T21:08:24Z';
 };
