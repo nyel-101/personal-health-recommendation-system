6fa4278785f8c7608cc7aaf3ba92baaeee816176
nyel-101 <dandevjr@gmail.com>
2025-05-26 22:06:33 +0000

    perf(api): fix auth tokens

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_59c0c8cb18f245dabbb81a10fd1f2de4 = () => {
-  return '2025-05-09T08:49:44Z';
+const node_f104cd8107984938a9631575c8fe1ef8 = () => {
+  return '2025-05-26T22:06:33Z';
 };
