4b2efc899e64077c11460a18f94ebb8181d23ea5
nyel-101 <dandevjr@gmail.com>
2025-06-03 16:26:18 +0000

    refactor(ui): sanitize auth tokens

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1d753b6d4fc141a89b6ed8b4fd6d01af = () => {
-  return '2025-05-17T10:55:16Z';
+const node_d101a80046fe4969aa24e02402c56218 = () => {
+  return '2025-06-03T16:26:18Z';
 };
