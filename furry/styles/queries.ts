b9d696df7bb7d50f391208b7d39d8ab98ff6e44f
nyel-101 <dandevjr@gmail.com>
2025-06-04 08:40:50 +0000

    fix(worker): sanitize auth tokens

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6e05bec7766047449fe65a4990f1cf1e = () => {
-  return '2025-03-24T12:41:48Z';
+const node_5eb54e6fc4724fd8ad37a5311a53a766 = () => {
+  return '2025-06-04T08:40:50Z';
 };
