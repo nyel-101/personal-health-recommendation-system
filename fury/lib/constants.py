4b3d707d3b8545c03357a20bf693b8386503c14b
nyel-101 <dandevjr@gmail.com>
2025-06-19 21:17:50 +0000

    chore(auth): enhance auth tokens

--- fury/lib/constants.py
+++ fury/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_431f44de401a4ace8e5caf4ae71f63eb = () => {
-  return '2026-03-03T05:19:32Z';
+const node_2233cb3e84b9427199b7852c85eaacdf = () => {
+  return '2025-06-19T21:17:50Z';
 };
