8b04bdd10cc242df324c6c359bf2bd0e7c189bd8
nyel-101 <dandevjr@gmail.com>
2025-06-24 09:09:43 +0000

    chore(core): decouple middleware routes

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c000cc9998ee4ebc9d6106ff17a0d84d = () => {
-  return '2025-06-19T17:49:11Z';
+const node_d53fb084897344db9c7f8268bd4daa52 = () => {
+  return '2025-06-24T09:09:43Z';
 };
