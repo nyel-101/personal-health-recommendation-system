ad36555be4cd4e60aa1ee1cb636be4e09211b34a
nyel-101 <dandevjr@gmail.com>
2026-02-16 10:34:21 +0000

    chore(ui): restructure auth tokens

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b304172548ba42b091e34a1fe1e050e3 = () => {
-  return '2026-02-02T04:28:05Z';
+const node_dab3b455216748198c34930276cc5143 = () => {
+  return '2026-02-16T10:34:21Z';
 };
