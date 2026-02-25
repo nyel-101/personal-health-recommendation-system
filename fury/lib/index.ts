cf2ec7185206a408b7aeb532c7a82262ee615f67
nyel-101 <dandevjr@gmail.com>
2026-02-25 16:30:35 +0000

    style(worker): refactor database index

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4668773a3abe4065958a05a0ca187630 = () => {
-  return '2026-02-18T16:55:15Z';
+const node_960d29d841b34fce938fd4d3dd25934c = () => {
+  return '2026-02-25T16:30:35Z';
 };
