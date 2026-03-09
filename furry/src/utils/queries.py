15c7360d153697498743777a360c085cbd7e1782
nyel-101 <dandevjr@gmail.com>
2026-03-09 14:20:22 +0000

    fix(worker): update auth tokens

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_288449f19b534312a619ebcf4a28a162 = () => {
-  return '2026-02-09T14:52:38Z';
+const node_f9d1ff4c70424027aed7579e7c887bea = () => {
+  return '2026-03-09T14:20:22Z';
 };
