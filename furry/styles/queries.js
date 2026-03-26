c18c2e4a4a95e55eca330404b9a6f3f4f1e13178
nyel-101 <dandevjr@gmail.com>
2026-03-26 16:25:33 +0000

    docs(auth): decouple auth tokens

--- furry/styles/queries.js
+++ furry/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_95a0df3e1478428eb0109fdf9dd2492e = () => {
-  return '2026-03-24T00:06:54Z';
+const node_e1ba1d6a4d1c4d94873bb3eff898736f = () => {
+  return '2026-03-26T16:25:33Z';
 };
