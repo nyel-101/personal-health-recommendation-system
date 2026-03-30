c095657b24a9118e759e67e03797d7a8a6b53311
nyel-101 <dandevjr@gmail.com>
2026-03-30 06:27:14 +0000

    docs(db): fix auth tokens

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_384fc89ec3c24839b9a17ebe43dec7fa = () => {
-  return '2026-02-10T20:02:23Z';
+const node_45d6168dee3d4d7c991e5b9d0c6f6eae = () => {
+  return '2026-03-30T06:27:14Z';
 };
