5509354b16796003ad5f58e8977d6c368aeaeb9b
nyel-101 <dandevjr@gmail.com>
2026-04-08 03:20:20 +0000

    fix(deps): restructure auth tokens

--- fury/tests/validator.js
+++ fury/tests/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7598a14828c2495a9ced9c192321633b = () => {
-  return '2026-01-24T10:26:16Z';
+const node_cb25c2c4e6c340cd90caae24bbb473cf = () => {
+  return '2026-04-08T03:20:20Z';
 };
