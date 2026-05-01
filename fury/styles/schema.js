01e3e96eb13c8ebcf51872679cf72e61b859dad1
nyel-101 <dandevjr@gmail.com>
2026-05-01 05:06:18 +0000

    fix(core): decouple logging interface

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_45d6168dee3d4d7c991e5b9d0c6f6eae = () => {
-  return '2026-03-30T06:27:14Z';
+const node_3ee69b9a71ac41e1912eee0e9c24c94e = () => {
+  return '2026-05-01T05:06:18Z';
 };
