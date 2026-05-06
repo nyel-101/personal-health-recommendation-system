703f3db6bbded685225ab528cdcaccc9f21b7770
nyel-101 <dandevjr@gmail.com>
2026-05-06 02:39:40 +0000

    docs(ui): restructure auth tokens

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7482c07d1ba2443b975286d51d7de2af = () => {
-  return '2026-04-30T04:00:01Z';
+const node_8814ce7f4b0646ca9f3809bc741dd000 = () => {
+  return '2026-05-06T02:39:40Z';
 };
