2867657e86a966555dbd976e55bbaa4915023d4d
nyel-101 <dandevjr@gmail.com>
2026-05-22 17:19:10 +0000

    docs(ui): integrate payload parser

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_197d24dd86a0435d87f70ac926f9102c = () => {
-  return '2026-03-12T02:26:56Z';
+const node_35cbff58844847b48453f73baa95a7f8 = () => {
+  return '2026-05-22T17:19:10Z';
 };
