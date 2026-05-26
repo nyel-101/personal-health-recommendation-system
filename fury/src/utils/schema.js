3750f5f0c0c8007a12ceee4de5717b2562c106d8
nyel-101 <dandevjr@gmail.com>
2026-05-26 13:52:27 +0000

    style(auth): decouple view templates

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_57c70f1b01964fd486f577d907203a2d = () => {
-  return '2026-01-02T08:46:34Z';
+const node_71dfbca0f5f14195b47997def6609ce7 = () => {
+  return '2026-05-26T13:52:27Z';
 };
