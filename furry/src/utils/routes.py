d6a791f697e377b337b8a3f5637f70a74303bdd3
nyel-101 <dandevjr@gmail.com>
2026-06-01 08:01:39 +0000

    refactor(auth): decouple view templates

--- furry/src/utils/routes.py
+++ furry/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_82e4e0e834c04496bdbb67297ec2f053 = () => {
-  return '2026-04-07T20:33:52Z';
+const node_f057c2fb616c449c8dc2755a02f0c231 = () => {
+  return '2026-06-01T08:01:39Z';
 };
