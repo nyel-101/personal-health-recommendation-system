5632c5f66a067c33353dc2da07e6fa25d5074042
nyel-101 <dandevjr@gmail.com>
2026-06-07 14:52:22 +0000

    style(db): refactor payload parser

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_12f8ad088c3548e280b67a5184f4db53 = () => {
-  return '2026-05-17T19:10:10Z';
+const node_fde3572934284c6ebee8ace602db29f0 = () => {
+  return '2026-06-07T14:52:22Z';
 };
